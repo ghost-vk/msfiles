2739a305951f9d1d3dce509d9024ec6f6323826b
ghost-vk <ghost.vkv@gmail.com>
2023-08-16 08:06:05 +0300

    feat: add endpoint create_upload_url
    Endpoint do the same like rabbitmq handler of create_upload_url

--- src/modules/core/controllers/internal-rest.controller.ts
+++ src/modules/core/controllers/internal-rest.controller.ts
@@ -1,17 +1,29 @@
-import { BadRequestException, Controller, Get, Logger, Query } from '@nestjs/common';
+import { BadRequestException, Body, CACHE_MANAGER, Controller, Get, Inject, Logger, Post, Query } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { Task } from '@prisma/client';
+import { Cache } from 'cache-manager';
+import { nanoid } from 'nanoid';
 
+import { FileActionsEnum } from '../../config/actions';
+import { AppConfig } from '../../config/types';
 import { PrismaService } from '../../prisma/prisma.service';
+import { CreateUploadUrlPayload } from '../dtos/create-upload-url-payload.dto';
 import { GetObjectUrlDto } from '../dtos/get-object-url.dto';
 import { GetTasksDto } from '../dtos/get-task.dto';
 import { MinioService } from '../services/minio.service';
+import { CreateUploadUrlCacheData } from '../types';
 
 @Controller()
 export class InternalRestController {
   private readonly logger = new Logger(InternalRestController.name);
 
-  constructor(private readonly minioService: MinioService, private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly minioService: MinioService,
+    private readonly prisma: PrismaService,
+    private readonly config: ConfigService<AppConfig, true>,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
+  ) {}
 
   @Get('tasks')
   @ApiExcludeEndpoint()
@@ -42,5 +54,48 @@ export class InternalRestController {
     const url = await this.minioService.getObjectUrl(args.objectname, { bucket: args.bucket });
 
     return { url };
+  }
+
+  @Post('create_upload_url')
+  @ApiExcludeEndpoint()
+  async createUploadUrl(@Body() payload: CreateUploadUrlPayload): Promise<{ url: string }> {
+    try {
+      const data: CreateUploadUrlCacheData = {
+        action: payload.action,
+        uid: payload.uid,
+      };
+
+      if (payload.bucket) data.bucket = payload.bucket;
+
+      const key = nanoid(10);
+
+      await this.cache.set(key, JSON.stringify(data), 30000);
+      let route: string;
+
+      switch (payload.action) {
+        case FileActionsEnum.UploadFile: {
+          route = 'uploadFile';
+          break;
+        }
+        case FileActionsEnum.UploadImage: {
+          route = 'uploadImage';
+          break;
+        }
+        case FileActionsEnum.UploadVideo: {
+          route = 'uploadVideo';
+          break;
+        }
+      }
+
+      const url = this.config.get('BASE_URL') + `/${route}/${key}`;
+
+      this.logger.log(`File upload URL generated [${url}], key=[${key}].`);
+
+      return { url };
+    } catch (err) {
+      this.logger.error('Create upload url error.', err);
+
+      throw new Error('Create upload url error.');
+    }
   }
 }
